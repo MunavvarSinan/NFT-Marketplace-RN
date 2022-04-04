9203b39b40da4a3f239075140da4ac9079a6ac4a
MunavvarSinan <munavvarsinan8@gmail.com>
2022-04-05 03:14:31 +0530

    initial commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import { CircleButton, RectButton } from './Button';
+
+export { default as FocusedStatusBar } from './FocusedStatusBar';
+export { default as NFTCard } from './NFTCard';
+export { default as HomeHeader } from './HomeHeader';
+export { default as DetailsDesc } from './DetailsDesc';
+export { default as DetailsBid } from './DetailsBid';
+export { CircleButton, RectButton };
